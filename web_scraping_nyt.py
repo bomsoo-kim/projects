2abe73095274914c34d8c192e1e214579a8eccb9
bomsoo-kim <46009363+bomsoo-kim@users.noreply.github.com>
2019-05-21 12:35:36 -0400

    Update web_scraping_nyt.py

--- web_scraping_nyt.py
+++ web_scraping_nyt.py
@@ -16,7 +16,7 @@ def text_mining_step1():
 
         pages = soup.find_all(name = 'a', string = re.compile("Part")) # NEED TO MODIFY
         for p in pages:
-            link = re.findall(r'[^/]+html', p['href'])[0] # NEED TO MODIFY
+            link = str(re.findall(r'[^/]+html', p['href'])[0]) # NEED TO MODIFY
             urls.append(url+link)
     #         print(url+link)
     
@@ -28,7 +28,8 @@ def text_mining_step2(urls):
     links = []
     titles = []
     for i, url in enumerate(urls):
-        print('%s / %s: '%(i,len(urls)), url)
+        if i%100 == 0:
+            print('%s / %s: '%(i,len(urls)), url)
 
         html = requests.get(url)
         soup = BeautifulSoup(html.text, 'html.parser')
@@ -36,11 +37,13 @@ def text_mining_step2(urls):
 
         pages = soup.select('ul#headlines li a') # NEED TO MODIFY
         for p in pages:
-            link = p['href'] # NEED TO MODIFY
-            title = p.string # NEED TO MODIFY
+            # MEMORY ISSUE SOLVED: https://stackoverflow.com/questions/11284643/python-high-memory-usage-with-beautifulsoup
+            link = str(p['href']) # NEED TO MODIFY
+            title = str(p.string) # NEED TO MODIFY
 
             links.append(link)
             titles.append(title)
         #     print(link, title)
+        
     return links, titles
-links, titles = text_mining_step2(urls)
+links, titles = text_mining_step2(urls) # it takes about 2 hours
