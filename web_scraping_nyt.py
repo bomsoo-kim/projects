5fb400927b418625d1105b4f4081371db81b44b0
bomsoo-kim <46009363+bomsoo-kim@users.noreply.github.com>
2019-06-05 11:11:41 -0400

    Update web_scraping_nyt.py

--- web_scraping_nyt.py
+++ web_scraping_nyt.py
@@ -56,50 +56,76 @@ def data_mining_step3(urls, url_links = [], yyyymmdd = [], authors = [], titles 
         url = urls[i]
         if (i%10 == 0) | (i == START_INDEX):
             print('%s / %s: '%(i,len(urls)), url)
-            
         html = requests.get(url)
         soup = BeautifulSoup(html.text, 'html.parser')
 #         print(soup.prettify())
-
-        #---- url_link -----------------------------------
-        if len(re.findall(r'/(\d{4})/(\d{2})/(\d{2})/', url)) > 0:
-            url_links_0 = url
-        else:
-            url_links_0 = str(soup.html['itemid'])
-
-        #---- date -----------------------------------
-        (y,m,d) = re.findall(r'/(\d{4})/(\d{2})/(\d{2})/', url_links_0)[0]
-        yyyymmdd_0 = int(y+m+d)
-
-        #---- author -----------------------------------
-        author = soup.find_all(name='meta', attrs={'name':['byl', 'author']})
-        authors_0 = re.findall('(?:^[Bb][Yy])?\s*(.*)', author[0]['content'])[0] # remove 'By ...', if any
-
-        #---- title -----------------------------------
-        title = soup.find_all(name='meta', attrs={'property':'og:title'})
-        titles_0 = str(title[0]['content'])
         
-        #---- story body -----------------------------------
-        paras = soup.find_all(name = 'p', class_ = re.compile('.+')) # find all tags with "p + class=..."
-
-        class_names = [p['class'][0]for p in paras] # extract class names from the tags
-
-        if 'story-body-text' in class_names: # determine the class name for the main story body
-            story_body_class_name = 'story-body-text'
-        else:
-            story_body_class_name = most_frequent(class_names) # the most frequent class name
-
-        bodies_0 = ''
-        for p in paras:
-            if story_body_class_name in p['class']: # extract text only from the main story body
-                bodies_0 = bodies_0 + p.get_text() + '\n'
+        if (soup.find('html') is not None) and (len(soup.find_all(name='meta', attrs={'name':'errorpage'})) == 0) and ('http://theater.nytimes.com/mem/theater/treview.html' not in url) and ('http://movies.nytimes.com/movie/review' not in url): 
+            #---- url_link -----------------------------------
+            if len(re.findall(r'/(\d{4})/(\d{2})/(\d{2})/', url)) > 0:
+                url_links_0 = url
+            else:
+                url_link = soup.find_all(name='meta', attrs={'property':'og:url'}) # ex) <meta property="og:url" content="https://www.nytimes.com/1852/01/31/archives/financial.html" />
+                url_links_0 = str(url_link[0]['content'])
                 
-        #-- add all ----------------------------------        
-        url_links.append(url_links_0)
-        yyyymmdd.append(yyyymmdd_0)
-        authors.append(authors_0)
-        titles.append(titles_0)
-        bodies.append(bodies_0)
+            #---- date -----------------------------------
+            if len(re.findall(r'/(\d{4})/(\d{2})/(\d{2})/', url_links_0)) > 0:
+                (y,m,d) = re.findall(r'/(\d{4})/(\d{2})/(\d{2})/', url_links_0)[0] 
+            else:
+                date = soup.find_all(name='meta', attrs={'itemprop':'datePublished'}) # ex) <meta data-rh="true" property="article:published" itemprop="datePublished" content="2018-06-19T20:25:49.000Z"/>
+                (y,m,d) = re.findall(r'(\d{4})-(\d{2})-(\d{2})', date[0]['content'])[0] 
+            yyyymmdd_0 = int(y+m+d)
+            
+            #---- author -----------------------------------
+            author = soup.find_all(name='meta', attrs={'name':['byl', 'author']})
+            authors_0 = re.findall('(?:^[Bb][Yy])?\s*(.*)', author[0]['content'])[0] # remove 'By ...', if any
+
+            #---- title -----------------------------------
+            title = soup.find_all(name='meta', attrs={'property':'og:title'})
+            if len(title) == 0:
+                title = soup.find_all(name='meta', attrs={'name':'hdl'}) # <meta name="hdl" content="No Rest for a Feminist Fighting Radical Islam">
+            titles_0 = str(title[0]['content'])
+
+            #---- story body -----------------------------------
+            paras = soup.find_all(name = 'p')
+            l_p_attrs_keys = [list(p.attrs.keys()) for p in paras]
+            mf_p_attrs_key = most_frequent_list_of_list(l_p_attrs_keys) # most frequent <p> tag attribute key
+
+            class_names = [p['class'][0]for p in paras if 'class' in p.attrs.keys()] # extract class names from the tags
+            if (len(class_names) > 0) and ('story-body-text' in class_names):
+                p_attrs_key = 'class'
+                p_attrs_value = 'story-body-text'
+            elif (len(class_names) > 0):
+                p_attrs_key = 'class'
+                p_attrs_value = most_frequent(class_names) # the most frequent class name
+            else:
+                p_attrs_key = 'itemprop'
+                p_attrs_value = 'articleBody'
+
+            bodies_0 = ''; bodies_1 = ''
+            for p in paras:
+                if (p_attrs_key in p.attrs.keys()) and (p_attrs_value in p[p_attrs_key]): # extract text only from the main story body
+                    bodies_0 = bodies_0 + p.get_text() + '\n'
+                if (mf_p_attrs_key == []) and (list(p.attrs.keys()) == []): 
+                    bodies_1 = bodies_1 + p.get_text() + '\n'
+
+            if (len(bodies_1) > len(bodies_0)) and (len(bodies_1) > 200): # replace the story body if the condition is met
+                bodies_0 = bodies_1
+                
+            #-- add all ----------------------------------        
+            url_links.append(url_links_0)
+            yyyymmdd.append(yyyymmdd_0)
+            authors.append(authors_0)
+            titles.append(titles_0)
+            bodies.append(bodies_0)
+        else:
+#             print('Nothing added because of one or more errors: ', url)
+            url_links.append('')
+            yyyymmdd.append('')
+            authors.append('')
+            titles.append('')
+            bodies.append('')            
+            
     return url_links, yyyymmdd, authors, titles, bodies
 
 #---------------------------------------------------------------------------------
